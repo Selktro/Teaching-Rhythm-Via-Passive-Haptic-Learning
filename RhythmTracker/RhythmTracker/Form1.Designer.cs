8825b3a95a6aafde65fa3f6e5ef2096078ad062e
Selktro <73837134+Selktro@users.noreply.github.com>
2022-04-25 12:05:07 +0200

    Add files via upload
    The program used to track keyboard inputs

---
+++ RhythmTracker/RhythmTracker/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace RhythmTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.recLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.idBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(117, 90);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(136, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start Recording";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            this.startButton.KeyDown += new System.Windows.Forms.KeyEventHandler(this.buttonPressed);
+            this.startButton.KeyUp += new System.Windows.Forms.KeyEventHandler(this.buttonLifted);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(117, 169);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(136, 23);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "Stop Recording";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // recLabel
+            // 
+            this.recLabel.AutoSize = true;
+            this.recLabel.ForeColor = System.Drawing.Color.Red;
+            this.recLabel.Location = new System.Drawing.Point(114, 116);
+            this.recLabel.Name = "recLabel";
+            this.recLabel.Size = new System.Drawing.Size(71, 13);
+            this.recLabel.TabIndex = 2;
+            this.recLabel.Text = "Not recording";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter ID:";
+            // 
+            // idBox
+            // 
+            this.idBox.Location = new System.Drawing.Point(117, 38);
+            this.idBox.Name = "idBox";
+            this.idBox.Size = new System.Drawing.Size(136, 20);
+            this.idBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 284);
+            this.Controls.Add(this.idBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.recLabel);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Rhythm Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.buttonPressed);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.buttonLifted);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label recLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox idBox;
+    }
+}
+
---
+++ RhythmTracker/RhythmTracker/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RhythmTracker
+{
+    public partial class Form1 : Form
+    {
+        private bool recording = false;
+        //private bool firstPress = true;
+        private List<string> pressedKeys = new List<string>();
+        private List<keyPress> keyList = new List<keyPress>();
+        private long firstPress = 0;
+        private DateTimeOffset now;
+        private TextWriter txt;
+        private long lastPressed = 0;
+
+        private class keyPress
+        {
+            public string keyPressed = "";
+            public long pressTime = 1;
+            public long releaseTime = 1;
+
+            public keyPress(string keyPressed, long pressTime, long releaseTime)
+            {
+                this.keyPressed = keyPressed;
+                this.pressTime = pressTime;
+                this.releaseTime = releaseTime;
+            }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPressed(object sender, KeyEventArgs e)
+        {
+            if (recording)
+            {
+                if (!pressedKeys.Contains(e.KeyCode.ToString()))
+                {
+                    pressedKeys.Add(e.KeyCode.ToString());
+                    if (firstPress == 0)
+                    {
+                        now = DateTimeOffset.UtcNow;
+                        firstPress = now.ToUnixTimeMilliseconds();
+                    }
+                    //else
+                    //{
+                    //    firstPress = releaseTime;
+                    //}
+                    now = DateTimeOffset.UtcNow;
+                    keyList.Add(new keyPress(e.KeyCode.ToString(), now.ToUnixTimeMilliseconds(), 0));
+                    //pressTime = now.ToUnixTimeMilliseconds();
+                    //keyPressed = e.KeyCode.ToString();
+                }
+            }
+        }
+
+        private void buttonLifted(object sender, KeyEventArgs e)
+        {
+            if (recording)
+            {
+                keyPress key = null;
+                foreach (keyPress k in keyList){
+                    if (k.keyPressed.Equals(e.KeyCode.ToString())){
+                        key = k;
+                        break;
+                    }
+                }
+                keyList.Remove(key);
+                now = DateTimeOffset.UtcNow;
+                key.releaseTime = now.ToUnixTimeMilliseconds();
+                trackTime(key);
+                pressedKeys.Remove(e.KeyCode.ToString());
+            }
+        }
+
+        private void trackTime(keyPress kp)
+        {
+            long waitTime = kp.pressTime - firstPress;
+            long endtime = kp.releaseTime - firstPress;
+            long keydownTime = kp.releaseTime - kp.pressTime;
+            long pausesince = kp.pressTime - lastPressed;
+            if(lastPressed == 0)
+            {
+                pausesince = 0;
+            }
+            txt.Write("Pressed from: " + waitTime + " to " + endtime + "\n");
+            txt.Write("Pressed key '" + kp.keyPressed + "' for " + keydownTime + " milliseconds\n");
+            txt.Write("Pause since previous note: " + pausesince  + "\n\n");
+            lastPressed = kp.releaseTime;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (!recording)
+            {
+                firstPress = 0;
+                recording = true;
+                string id;
+                if (string.IsNullOrEmpty(idBox.Text))
+                {
+                    id = "noID";
+                }
+                else
+                {
+                    id = idBox.Text;
+                }
+                txt = new StreamWriter(".\\" + id + ".txt", true);
+                recLabel.Text = "Recording";
+                recLabel.ForeColor = Color.Green;
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            if (recording)
+            {
+                recording = false;
+                txt.Write("\n\n");
+                txt.Close();
+                recLabel.Text = "Not recording";
+                recLabel.ForeColor = Color.Red;
+            }
+        }
+    }
+}
